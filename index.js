ee003716866db40973acdcfd44f867d929a30c99
Leonardo <mr_kunkel@hotmail.com>
2022-01-12 11:52:22 -0600

    agregado de propiedades

--- index.js
+++ index.js
@@ -12,3 +12,32 @@ console.log(mexico.comida);
 
 // BRACKET NOTATION - NOTACIÓN POR CORCHETES
 console.log(mexico["comida"]);
+
+const olympicRecords = {
+    athletics100Men: "Justin",
+    athleticsLongJumpMen: "Mike",
+    swinning200Men: "Phelps",
+    "swimming 200 Women": "Ariarne Titmus"
+}
+
+console.log(olympicRecords.athletics100Men);
+console.log(olympicRecords["athletics100Men"]);
+
+console.log(olympicRecords["swimming 200 Women"]);
+
+
+// B. AGREGAR PROPIEDADES
+const refresco = {
+    nombre: "Coca-Cola",
+    color: "Oscuro",
+    precio: 12,
+    tamanos: [600, 1000]
+}
+
+refresco.envase = "Plástico"
+
+console.log("Objeto de refresco: ", refresco);
+
+refresco["competidor"] = "Pepsi"
+
+console.log(refresco);
