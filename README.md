5786a772204f469e507bb419446fbc53e2dd8200
Leonardo <mr_kunkel@hotmail.com>
2022-01-12 11:33:33 -0600

    Acceso a propiedades

---
+++ README.md
@@ -0,0 +1,5 @@
+#DOCUMENTACIÓN
+
+## Prerequisitos
+- Instalar como extensión en Visual Studio Code:
+    - [Code Runner](https://marketplace.visualstudio.com/items?itemName=formulahendry.code-runner)
---
+++ index.js
@@ -0,0 +1,14 @@
+// OBJETOS
+// NO PRIMITIVOS
+// ESTÁN COMPUESTOS POR PROPIEDADES Y VALORES
+
+const mexico = {
+    comida: "Tacos" // Propiedad "comida" y valor "Tacos"
+}
+console.log(mexico);
+// A. ACCEDER A LAS PROPIEDADES
+// DOT NOTATION - NOTACIÓN DE PUNTO
+console.log(mexico.comida);
+
+// BRACKET NOTATION - NOTACIÓN POR CORCHETES
+console.log(mexico["comida"]);
