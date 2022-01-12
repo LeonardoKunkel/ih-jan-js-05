2a98b349190432f5b7685318ac5422772786e996
Leonardo <mr_kunkel@hotmail.com>
2022-01-12 13:29:02 -0600

    ejercicio de objetos

--- index.js
+++ index.js
@@ -122,41 +122,97 @@ estudiantesIH = [
 
 console.log(estudiantesIH);
 
-let arrHalo = [
-    {
-        name: "John",
-        species: "Human",
-        kills: 87634876587923465,
-        alias: [
-            "Master chief",
-            "Demon",
-            "Sierra 117"
+// NESTED DATA - ESTRCUTURAS
+const schoolSystem = {
+    schools: [
+      {
+        name: "Fake School 1",
+        classRooms: [
+          {
+            teacher: { firstName: 'Marcelino', lastName: 'Padberg', age: 25 },
+            students: [
+                { firstName: 'Aliyah', lastName: 'Schulist', age: 18 },
+                { firstName: 'Cleveland', lastName: 'Towne', age: 28 },
+                { firstName: 'Jan', lastName: 'Quitzon', age: 18 },
+                { firstName: 'Alaina', lastName: 'Runolfsdottir', age: 18 },
+                { firstName: 'Gerhard', lastName: 'Bergstrom', age: 23 }
+              ]
+          },
+          {
+            teacher: { firstName: 'Edwardo', lastName: 'Schowalter', age: 28 },
+            students: [
+              { firstName: 'Manley', lastName: 'Doyle', age: 18 },
+              { firstName: 'Maximilian', lastName: 'Gleichner', age: 19 },
+              { firstName: 'Sid', lastName: 'Rohan', age: 30 },
+              { firstName: 'Catalina', lastName: 'Hilpert', age: 27 },
+              { firstName: 'Gerald', lastName: 'O\'Keefe', age: 26 }
+            ]
+          }
         ]
-    },
-    {
-        name: "Thel 'Vadam",
-        species: "Sangheili",
-        kills: 765785,
-        alias: [
-            "Arbiter",
-            "split face"
+       },
+       {
+        name: "Fake School 2",
+        classRooms: [
+          {
+            teacher: { firstName: 'Lucas', lastName: 'Schroeder', age: 29 },
+            students: [
+              { firstName: 'Giuseppe', lastName: 'Hegmann', age: 24 },
+              { firstName: 'Jennyfer', lastName: 'Hane', age: 19 },
+              { firstName: 'Mikayla', lastName: 'Braun', age: 23 },
+              { firstName: 'Rickie', lastName: 'White', age: 22 },
+              { firstName: 'Rose', lastName: 'Collins', age: 30 }
+            ]
+          },
+          {
+            teacher: { firstName: 'Green', lastName: 'Sauer', age: 25 },
+            students: [
+              { firstName: 'Melany', lastName: 'Welch', age: 25 },
+              { firstName: 'Paxton', lastName: 'Corkery', age: 22 },
+              { firstName: 'Nellie', lastName: 'Hauck', age: 26 },
+              { firstName: 'Eunice', lastName: 'Hirthe', age: 26 },
+              { firstName: 'Aylin', lastName: 'Barrows', age: 26 }
+             ]
+           }
+         ]
+       },
+       {
+        name: "Fake School 3",
+        classRooms: [
+          {
+            teacher: { firstName: 'Nikko', lastName: 'Crist', age: 42 },
+            students: [
+              { firstName: 'Christop', lastName: 'Hahn', age: 26 },
+              { firstName: 'Newell', lastName: 'Kemmer', age: 27 },
+              { firstName: 'Katheryn', lastName: 'Heller', age: 26 },
+              { firstName: 'Saul', lastName: 'Heathcote', age: 20 },
+              { firstName: 'Maudie', lastName: 'Haley', age: 30 }
+            ]
+          },
+          {
+            teacher: { firstName: 'Nathanael', lastName: 'Hansson', age: 50 },
+            students: [
+              { firstName: 'Jensen', lastName: 'Reichel', age: 21 },
+              { firstName: 'Lois', lastName: 'Kulas', age: 28 },
+              { firstName: 'Caterina', lastName: 'Wolff', age: 28 },
+              { firstName: 'Dahlia', lastName: 'Collier', age: 24 },
+              { firstName: 'Linwood', lastName: 'Langosh', age: 26 }
+            ]
+          }
         ]
-    },
-    {
-        name: "Miranda Keyes",
-        species: "Human",
-        kills: 3,
-        alias: "Keyes"
-    },
-    {
-        name: "Tartarus",
-        species: "Monkeys?",
-        kills: 666,
-        alias: "Space ape"
-    }
-]
+      }
+    ]
+  };
+  console.log(schoolSystem.schools[2]); // => Christop
+  console.log(schoolSystem.schools[2].classRooms[1].teacher.firstName); // => Christop
+  console.log(schoolSystem.schools[2].classRooms[1].students[3].firstName); // => Christop
+  schoolSystem.schools[1].classRooms[0].students.push({
+      firstName: "D. Lozano",
+      lastName: "Rodríguez",
+      age: 78
+  });
+  console.log(schoolSystem.schools[1].classRooms[0].students);
 
-console.log(arrHalo[0].alias[1]);
-console.log(arrHalo[1].alias[0]);
-console.log(arrHalo[2].species);
-console.log(arrHalo[3].alias);
+// Retrieve the "Fake School 3" object
+// Retrieve the teacher with the first name of "Nathanael"
+// Retrieve the student with the first name of "Saul"
+// Add a student with the name of Lucille D. Lozano to Fake School 2, in the first classroom.
